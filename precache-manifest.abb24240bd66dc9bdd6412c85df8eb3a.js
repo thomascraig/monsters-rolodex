2651afac16f3bd45cc440c86855811a38f977b1b
Thomas Craig <thomascraig@users.noreply.github.com>
2019-11-24 05:55:51 -0400

    Updates

---
+++ precache-manifest.abb24240bd66dc9bdd6412c85df8eb3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad7085d043d7f590623c131196bd002",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee9a0d877f32e4fc7dfa",
+    "url": "/monsters-rolodex/static/css/main.1255e273.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "ee9a0d877f32e4fc7dfa",
+    "url": "/monsters-rolodex/static/js/main.98fa153d.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
